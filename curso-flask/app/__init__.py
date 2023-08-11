c9135cf780593b1b9591e621ae8500233aadca83
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-08-10 23:55:42 -0300

    Reinicio Aulas Flask
    -Reiniciando aulas  flask

--- curso-flask/app/__init__.py
+++ curso-flask/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__,static_folder='static', template_folder='templates')
+db = SQLAlchemy(app)
 app.secret_key = "123456"
 
 
