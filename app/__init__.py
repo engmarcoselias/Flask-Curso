173d810f3e058f84fff6439a09b925ff9c099336
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-08-16 00:03:01 -0300

    Migração SQL, Rotas
    -Criação de tabelas
    -Criando rotas-
    -Arquivo de configuração separado

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///storage.db"
-
+app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
+
+from app.controller import default
 from  app.model import table
+
 
 with app.app_context():
         db.create_all()
--- app/controller/default.py
+++ app/controller/default.py
@@ -1,5 +1,18 @@
 from app import app
 
+#---------Definindo rotas--------------
+
+@app.route("/index")
 @app.route("/")
 def index():
     return "Inicio"
+
+#--------passando parametros----------------
+@app.route("/test", defaults={'name': None})
+#@app.route("/test/")
+@app.route("/test/<name>")
+def test(name):
+    if name:
+        return "Olá, %s!" % name
+    else:
+        return "Olá usuario!"
